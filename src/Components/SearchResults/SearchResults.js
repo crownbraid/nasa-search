0e07e3cef7b6f2ba24d22b8fca8d0f715df9ca15
Elynor <elynorfreyss@gmail.com>
2018-12-06 15:34:19 -0500

    Refactor Results Array Mapping

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -4,20 +4,24 @@ import Result from '../Result/Result';
 import NoResults from '../NoResults/NoResults';
 
 const SearchResults = props => {
-  let results;
-  if (Array.isArray(props.results) && props.results.length > 0) {
-    results = props.results.map((result, id) => {
-      return (result.links && !result.links[0].href.endsWith('srt') && !result.links[0].href.endsWith('vtt')) 
-              ? <Result data={result} key={result.href} {...props}/> 
-              : null
-    });
-  }
-
-	return (
+  const results = mapResultsToJSX(props);
+	
+  return (
     <ul className="searchResults">
       {results ? results : <NoResults />}
     </ul>
   );
 };
+
+const mapResultsToJSX = props => {
+  if (Array.isArray(props.results) && props.results.length > 0) {
+    return props.results.map((result, id) => {
+      return (result.links && !result.links[0].href.endsWith('srt') && !result.links[0].href.endsWith('vtt')) 
+              ? <Result data={result} key={result.href} {...props}/> 
+              : null
+    });
+  }
+  return false;
+}
 
 export default SearchResults;
